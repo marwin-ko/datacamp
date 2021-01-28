78e7b01e3ece9d577c2e0b416f56db39a10e96cc
Marwin Ko <marwin.r.ko@gmail.com>
2021-01-28 15:23:54 -0800

    Update boto3_sns.py

--- boto3_sns.py
+++ boto3_sns.py
@@ -3,8 +3,12 @@ region_name = 'us-east-1',
 aws_access_key_id = AWS_KEY_ID,
 aws_secret_access_key=AWS_SECRET)
 
-# Create Topic
 
+# Summary
+# create topic, add subscribers to that topic, leverage that topic to mass send (publish) messages.
+
+# Create Topic
+topic_arn = sns.create_topic(Name = 'topic_notification')['TopicArn']
 
 # subscribe phone
 response = sns.subscribe(
@@ -26,14 +30,21 @@ subs = pd.DataFrame(response['Subscriptions'])
 
 # unsubscribe
 for sub in subs:
-response = sns.unsubscribe(SubscriptionArn = sub['SubscriptionArn']
+response = sns.unsubscribe(SubscriptionArn = sub['SubscriptionArn'])
 
 # publish to a topic
 response = sns.publish(
 TopicArn = 'arn',
-Message = 'Body test of SMS or e-mail',
-Subject = 'Subject Line for Email'
+Message = 'Body text of SMS or e-mail',
+Subject = 'Subject Line for Email'  # not visible for texts
 )
+                           
+# sending single SMS message
+# (no need for topicArn or subscription)
+# good for one-off message (not good for maintainability)
+response = sns.publish(
+  PhoneNumber  = '+1234567890',
+  Message = 'Body text')
 
 
 
