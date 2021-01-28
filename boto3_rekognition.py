f1b3ac668bfe2bf554e6a5006a1a5706891cc28c
Marwin Ko <marwin.r.ko@gmail.com>
2021-01-28 15:42:25 -0800

    Update boto3_rekognition.py

--- boto3_rekognition.py
+++ boto3_rekognition.py
@@ -3,7 +3,11 @@
 
 # Features
 # - detecting objects
+#   - count instances of objects
 # - extracting text from images
+  # "TextDetections" can be 2 types: "LINE" or "WORD"
+  # Line detection is a line of word (can span over multiple adjacent object)
+  # Word detection is just the word by itself.
 
 
 # Init client
@@ -11,3 +15,27 @@ rekog = boto3.client('rekognition',
 region_name = 'us-east-1',
 aws_access_key_id = AWS_KEY_ID,
 aws_secret_access_key = AWS_SECRET)
+
+
+# detect objects from image
+response = rekog.detect_labels(
+  Image = {'S3Object' : 
+           {
+             'Bucket': 'my-bucket',
+             'Name': 'image.jpg'
+           }
+          },
+  MaxLabels = 10,
+  MinConfidence = 95  # min percent confidence
+)
+
+
+# extract text from image
+response = rekog.detect_labels(
+    Image = {'S3Object' :
+             {
+               'Bucket': 'my-bucket',
+               'Name': 'image.jpg'
+             }
+            }
+)
