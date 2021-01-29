05c04afce7aba30eda8ec4650926c21c364771d8
Marwin Ko <marwin.r.ko@gmail.com>
2021-01-28 18:15:13 -0800

    Update boto3_translate.py

--- boto3_translate.py
+++ boto3_translate.py
@@ -11,10 +11,13 @@ SourceLanguage = 'auto', # this is using AWS resource "comprehend"
 TargetLanguageCode = 'es')
 
 
-# Comprehend (language type detection)
+# Comprehend (language type detection and sentiment)
 comprehend = boto3.client('comprehend',
 region_name = '',
 aws_access_key_id = AWS KEY_ID,
 aws_secret_access_key=AWS_SECRET)
 
 response = comprehend.detect_dominant_language(Text = 'Hola amigo!')
+top_lanuage = response['Languages'][0]['LanguageCode']
+
+sentiment = comprehend.detect_sentiment(Text = 'I love it!', LanguageCode='en')['Sentiment'])
