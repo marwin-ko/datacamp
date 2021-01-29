49e8fe1b312fe54da58a573a634ed581a8db764c
Marwin Ko <marwin.r.ko@gmail.com>
2021-01-29 13:44:36 -0800

    Update pyspark1.py

--- pyspark1.py
+++ pyspark1.py
@@ -9,3 +9,28 @@ spk_df.printSchema()
 # shows calcualuated result
 # spk_df.groupBy('Col1').mean('Col2').show()
 
+
+# example of standalone script
+from pyspark.sql import SparkSession
+if __name__ == "__main__":
+    spark = SparkSession.builder.getOrCreate()
+    spk_df = (spark
+        .read
+        .csv("./data.csv",
+             header=True,
+             inferSchema=True,
+             escape='"'))
+
+    spk_df = (spk_df
+        .withColumn("Height",
+                    spk_df.Height.cast("integer")))
+
+    print(spk_df
+        .groupBy('Year')
+        .mean('Height')
+        .orderBy('Year')
+        .show())
+# end of script
+
+
+
