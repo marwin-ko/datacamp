d23a63f94335101297b906c2446b5ea640659289
Marwin Ko <marwin.r.ko@gmail.com>
2021-02-23 22:13:41 -0800

    Create docstring__numpydoc.py

---
+++ docstring__numpydoc.py
@@ -0,0 +1,19 @@
+def function(arg1, arg2=42):
+  """
+  Description of what the function does.
+  
+  Parameters
+  ----------
+  arg1 : type
+    Description of arg1.
+  arg2 : int, optional
+    Write optional when an argument has a default value.
+    Default=42.
+    
+  Returns
+  -------
+  The type of the return value
+    Can include a description of the return value.
+    Replace "Returns" with "Yields" if this function is a generator.
+  """
+  return arg1 + arg2
