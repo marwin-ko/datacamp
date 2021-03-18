d7f2ca27514c0c736c4b612a50da1b8cc67a3e89
Marwin Ko <marwin.r.ko@gmail.com>
2021-03-18 12:53:27 -0700

    Update context_manager.py

--- context_manager.py
+++ context_manager.py
@@ -7,3 +7,14 @@
 # set up a context     set up tables w/ food & drink
 # run your code        you and your friends party
 # remove the contet    clean up and remove tables 
+
+
+# format
+with <context-manager>(<args>) as <variable-name>:
+  # run code
+  # this code is running "inside the context"
+
+
+# example 1
+with open('testfile.txt') as file:
+  text = file.read()
