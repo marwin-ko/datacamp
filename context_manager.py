afef11067f345d34225b6d92dd6754ce95fdb9e1
Marwin Ko <marwin.r.ko@gmail.com>
2021-03-18 12:46:35 -0700

    Create context_manager.py

---
+++ context_manager.py
@@ -0,0 +1,9 @@
+# Context Manager
+
+#
+# analogy
+#
+# context manager      caterer
+# set up a context     set up tables w/ food & drink
+# run your code        you and your friends party
+# remove the contet    clean up and remove tables 
