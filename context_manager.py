e21808dc360b932b5fa7e8fa32f7df594e7c7c7a
Marwin Ko <marwin.r.ko@gmail.com>
2021-03-18 12:59:50 -0700

    Update context_manager.py

--- context_manager.py
+++ context_manager.py
@@ -18,3 +18,19 @@ with <context-manager>(<args>) as <variable-name>:
 # example 1
 with open('testfile.txt') as file:
   text = file.read()
+
+  
+  
+# how to create a context manager (function implementation)
+# 1. defin a function
+# 2. (optional) add setup code
+# 3. Use "yield" keyword
+# 4. (optional) add teardown code
+# 5. Add the '@contextlib.contextmanager' decorator
+
+@contextlib.contextmanager
+def my_context():
+  # Add set up code
+  yield
+  # Add teardown code
+
