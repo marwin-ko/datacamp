6b258b85b9e9befef9b33a30245ae758b316dd55
Marwin Ko <marwin.r.ko@gmail.com>
2022-04-20 17:41:13 -0700

    Create oop_fundamentals.py

---
+++ oop_fundamentals.py
@@ -0,0 +1,9 @@
+object = state + behavior
+object = attributes(variables) + methods (functions)
+
+
+
+# Notes
+# use dir() to get all attributes and methods for an object
+# classes and objects both have attributes and methods
+  # The difference is that a class is an abstract template, while an object is a concrete representation of a class.
